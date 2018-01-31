f081293afdc1282e2222a3960ef35095af199a77
TinaBryan <tdmak63@gmail.com>
2018-01-31 16:14:41 -0500

    adding the getters and setters for the Cat clas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+
+    // create a new class Cat
+        // when the variables are written they have to be in the same order as they were created within the class
+        Cat cat = new Cat(4, true, "Calico");
+
+        System.out.println(cat.getColor());
+
     }
 }
