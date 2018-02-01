dc1ac203a4c0197eb964136f2e0a9385983e1d9c
TinaBryan <tdmak63@gmail.com>
2018-02-01 14:45:27 -0500

    Added Pencil class with constructors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-    // create a new class Cat
-        // when the variables are written they have to be in the same order as they were created within the class
+     //create a new class Cat
+//         when the variables are written they have to be in the same order as they were created within the class
         Cat cat = new Cat(4, true, "Calico");
 
         System.out.println(cat.getColor());
+
+        Pencil pencil = new Pencil(.7, "Dixion", true, "Orange", false);
+        Pencil pencil1 = new Pencil(.5, "PaperMate", false, "Yellow", true);
+
+        System.out.println("This is pencil: " + pencil.getColor());
+        System.out.println("This is pencil: " + pencil.getBrand());
+        System.out.println("This is pencil: " + pencil1.getBrand());
+        System.out.println("This is pencil: " + pencil1.getLeadSize());
 
     }
 }
